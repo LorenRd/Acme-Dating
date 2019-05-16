11b7e24fe1c984c5a624ff3bb72c52927b9ad593
Loren <lrondan95@gmail.com>
2019-05-16 22:48:55 +0200

    Repositorios

---
+++ Acme-Dating/src/main/java/repositories/TrophyRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Trophy;
+
+@Repository
+public interface TrophyRepository extends
+		JpaRepository<Trophy, Integer> {
+
+}
