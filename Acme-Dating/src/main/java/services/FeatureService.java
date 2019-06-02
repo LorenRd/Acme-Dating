6db4a9cd4f9bc5ff57aace202f9718ea58e064a4
Loren <lrondan95@gmail.com>
2019-06-02 19:38:38 +0200

    Tests

--- Acme-Dating/src/main/java/services/FeatureService.java
+++ Acme-Dating/src/main/java/services/FeatureService.java
@@ -137,4 +137,8 @@ public class FeatureService {
 		return result;
 	}
 
+	public void flush() {
+		this.featureRepository.flush();
+	}
+
 }
---
+++ Acme-Dating/src/test/java/services/FeatureServiceTest.java
@@ -0,0 +1,84 @@
+package services;
+
+import javax.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/junit.xml", })
+@Transactional
+public class FeatureServiceTest extends AbstractTest {
+
+	// Service under test
+
+	@Autowired
+	private FeatureService featureService;
+	/*
+	 *  Percentage of service tested:
+	 * 
+	 */
+	// --------------------------------------------------
+
+	/*
+	 * Requirement Tested: Un actor autenticado como empresa debe ser capaz de:
+	 *		4. Gestionar los extras.
+	 */
+
+	// Tests
+
+	/*
+	 * En este test se va a probar que solo puede crear un extra un usuario logueado como company
+	 */
+
+	@Test
+	public void createTestFail() {
+		final Object createTest[][] = { {
+			/*
+			 * Test negativo: User
+			 */
+			"user1", IllegalArgumentException.class,
+			
+			/*
+			 * Test negativo: admin
+			 */
+			"admin", IllegalArgumentException.class,
+
+			/*
+			 * Test negativo: Company
+			 */
+			"company1", null,
+
+		
+			} 
+		
+		};
+		
+		
+		for (int i = 0; i < createTest.length; i++)
+			this.CreateTemplate((String) createTest[i][0], (Class<?>) createTest[i][1]);
+	}
+
+
+	
+	
+	// Ancillary methods ------------------------------------------------------
+
+	private void CreateTemplate(final String actor, final Class<?> class1) {
+		Class<?> caught;
+		caught = null;
+		try {
+			this.authenticate(actor);
+			this.featureService.create();
+			this.unauthenticate();
+			this.featureService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(class1, caught);
+	}
+}
